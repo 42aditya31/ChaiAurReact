28017d9ac684ce0c2a331fe7a2ad8d9dbf0712f4
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-04-18 00:25:28 +0530

    creating the structure for the context-api functionality

---
+++ 08context-api/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1  className='text-9xl font-extrabold text-pink-400'> hello ji</h1>
+    </div>
+  )
+}
+
+export default App
