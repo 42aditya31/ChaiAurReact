5e47e72e045a47a217716ac148237b2a920c9369
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-04-17 22:22:55 +0530

    Creating a structure for react-router

---
+++ 07react-router/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    < >
+    
+    <div className='bg-black text-white h-screen'>
+      <h1 className='text-9xl font-extrabold font-stretch-75%'>hello ji </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
