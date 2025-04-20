e71e979d94e7333143a0d613d11d706f56607319
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-04-20 09:11:31 +0530

    Adding a Cotextapi to the todo-list

--- 10to-do-list/src/App.jsx
+++ 10to-do-list/src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Todoprovider } from "./context/TodoContext";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Todoprovider value={todos}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </Todoprovider>
   );
 };
 
