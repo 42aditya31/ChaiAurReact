96b1b44379eb1edb79a706ebfe27b2d9cb34b381
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-04-26 23:27:44 +0530

    adding the structure for the Redux-Toolkit

---
+++ 11redux-toolkit/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-pink-300 text-9xl underline font-bold '>hello jiii</h1>
+    </div>
+  )
+}
+
+export default App
