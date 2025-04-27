17300ad61b16151c9f7e601791574c1a07f83bd4
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-04-27 17:12:58 +0530

    Creating the structure for the PERSONAL_BLOG_WEBSITE

---
+++ 12blog/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-9xl font-bold text-pink-200'>hello jii</h1>
+    </div>
+  )
+}
+
+export default App
